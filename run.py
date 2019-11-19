ad1814a9b49c4e9aac1d7c5dfd357876d88f49c9
Kwaku <kwakuappiahadu@gmail.com>
2019-11-19 10:43:34 +0000

    added username & password variables

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import JWTManager
 app = Flask(__name__)
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Winter19@@localhost/app'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{username}:{password}@localhost/app'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'some-secret-string'
 
@@ -21,7 +21,7 @@ app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 def check_if_token_in_blacklist(decrypted_token):
     jti = decrypted_token['jti']
     return models.RevokedTokenModel.is_jti_blacklisted(jti)
-    
+
 @app.before_first_request
 def create_tables():
     db.create_all()
